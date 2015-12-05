95030bcedc4fd4c669e7fb452e214ca05df66eec
hlatki <hannahatkinson@gmail.com>
2015-12-05 14:31:42 -0800

    Add ability to specify whois server to use

--- src/clj_whois/core.clj
+++ src/clj_whois/core.clj
@@ -1,13 +1,23 @@
 (ns clj-whois.core
   (:import [org.apache.commons.net.whois WhoisClient]))
 
-(defn whois [url]
+
+(defn query
+  "Wraps WhoisClient.query.
+  whois-endpoint is the whois server you want to query
+  url is the domain name you want to look up
+
+  Examples:
+    (whois \"whois.nic.it\" \"google.it\")
+    (whois \"whois.iana.org\" \"com.\") -- this gets the whois server for the given tld
+  "
+  ([url] (whois WhoisClient/DEFAULT_HOST url))
+  ([whois-endpoint url]
   (let [wis (WhoisClient.)]
-    (. wis connect WhoisClient/DEFAULT_HOST)
+    (. wis connect whois-endpoint)
     (let [ret (try (. wis query url)
                    (catch java.io.IOException ex
                      (println ex)
                      "Failed"))]
       (. wis disconnect)
-      ret)))
-
+      ret))))
