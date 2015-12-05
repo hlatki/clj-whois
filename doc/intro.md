f6cc9543bef5191b8061976a6993d09f93ae39d4
hlatki <hannahatkinson@gmail.com>
2015-12-05 11:43:45 -0800

    Initial commit
    Output of lein new ...

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-whois
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_whois/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-whois.core-test
+  (:require [clojure.test :refer :all]
+            [clj-whois.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
