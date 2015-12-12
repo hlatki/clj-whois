ce69dd63dc9c8eba6eb76041de99d6478bc4f5ce
hlatki <hannahatkinson@gmail.com>
2015-12-12 14:33:25 -0800

    Cleanup and add super naive mapify method for response

--- src/clj_whois/parser.clj
+++ src/clj_whois/parser.clj
@@ -2,13 +2,64 @@
 (ns clj-whois.parser)
 
 
-(defmacro no-exception
-  "Return nil instead of throwing an exception"
-  [method object]
-  `(try (~method  ~object)
-       (catch Exception ~'e nil)))
+(defn- excep-helper
+  "Actually do the work for val-not-exception macro"
+  ([method object] (excep-helper method object nil))
+  ([method object val]
+   `(try (~method  ~object)
+       (catch Exception ~'e ~val))))
+
+(defmacro val-not-exception
+  "Try calling method on object and return the result.  If an exception is thrown
+  while evaluating method, return val instead. Returns nil if val isn't returned.
+  args: method object value
+  (value is optional)"
+  [& args]
+  (apply excep-helper args))
 
 
+;;(defmacro val-not-exception
+;;
+;;  ([method object] (val-not-exception &form &env method object 1))
+;;  ([method object v]
+;;  `(try (~method  ~object)
+;;       (catch Exception ~'e ~v))))
+
+
+;; new approach "mapify" the response and then standardize it
+;; regex for "key" in response
+(def whois-key-regex #"[\n\r\t]+[\w '-]+:")
+
+;; TODO: cleanup
+;; TODO: what to do about problem parsing the end
+;; TODO: strip newlines out of key
+(defn mapify-response
+  "Convert a WHOIS response to a map.
+  This is based on the assumption that \"keys\" are defined by
+  [\\n\\r\\t]+[\\w '-]+: and everything until the next key is
+  its value. This is an unproven assertion."
+  [response]
+  (let [matcher (re-matcher whois-key-regex response)]
+      (loop [prev-key nil
+             prev-key-end nil
+             key (re-find matcher)
+             resp-map {}]
+        (if (not key)
+          resp-map
+          (if (not prev-key)
+            (recur key (val-not-exception .end matcher (count response)) (re-find matcher) resp-map)
+            (recur key (val-not-exception .end matcher (count response)) (re-find matcher)
+                   (assoc resp-map prev-key (subs response prev-key-end (dec (val-not-exception .start matcher (count response)))))))))))
+
+
+
+
+
+
+
+
+
+;; First attempt query response for some attribute (e.g. "Domain")
 
 (defn find-value [attribute text]
   "Try to find an attribute in a response. Returns the raw list of matches for processing"
@@ -18,9 +69,10 @@
 
 
 ;; TODO: Maybe combine match in string for address/owners and leave as list for name servers etc?
-(defn process-matches [matches]
+(defn process-matches
   "Process list of matches from find-value.  If there is more than one match, they are combined
   (one match per line of resulting string"
+  [matches]
   (if (seq matches)
     (if (= (count matches) 1)
       (second (first matches))
@@ -28,11 +80,12 @@
       ;;(apply str (map #(str (second %) "\n") matches))) -- should probably leave as list?
     matches))
 
-(defn get-attribute [key attribute-name text]
+(defn get-attribute
   "Extract an attribute from a whois response.
   Example:
     (get-attribute :owner \"Owner\" whois-response)
     => {:owner \"euphoria GmbH\\neuphoria GmbH\\nFernkorngasse 10\\nVienna\\nAT\\n\"\"}"
+  [key attribute-name text]
   {key (process-matches (find-value attribute-name text))})
 
 
